2d69b56130445100c7e946a3edad7ccf2520aba6
Adil Sadik <sparky.005@gmail.com>
2020-07-22 10:30:08 -0400

    add google analytics ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-173278262-1`,
       },
     },
     `gatsby-plugin-feed`,
